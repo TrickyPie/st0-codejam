7f571fe127dbc1321457a790594943139041ddac
TrickyPie <klimova.pr@gmail.com>
2022-08-23 21:18:45 +0300

    init: init project

--- src/assets/Ancients/index.js
+++ src/assets/Ancients/index.js
@@ -1,7 +1,7 @@
-import azathoth from './Azathoth.jpg';
-import cthulhu from './Cthulthu.jpg';
-import iogSothoth from './IogSothoth.jpg';
-import shubNiggurath from './ShubNiggurath.jpg';
+import azathoth from './Azathoth.png';
+import cthulhu from './Cthulthu.png';
+import iogSothoth from './IogSothoth.png';
+import shubNiggurath from './ShubNiggurath.png';
 
 const ancients = {
   azathoth,
